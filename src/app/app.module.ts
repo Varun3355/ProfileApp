a7c7d7f3af2bcac1340a258c483156c79524fc02
Varun Manwani <manwanivarun961@gmail.com>
2022-07-05 19:16:56 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './Nav/about/about.component';
+import { EduComponent } from './Nav/edu/edu.component';
+import { SkillComponent } from './Nav/skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    EduComponent,
+    SkillComponent,
   ],
   imports: [
     BrowserModule,
